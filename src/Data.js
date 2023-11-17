43c4cd846a1d94587086529866477f76dd6e0911
oringjea@gmail.com <oringjea@gmail.com>
2023-11-17 09:16:09 +0800

    commit

---
+++ src/Data.js
@@ -0,0 +1,82 @@
+import react from "react";
+
+export const data = [
+    {
+        id: 1,
+        name: "KIM, NAMJOON",
+        place: "Seoul, Korea"
+    },
+    {
+        id: 2,
+        name: "KIM, SEOKJIN",
+        place: "Seoul, Korea"
+    },
+    {
+        id: 3,
+        name: "MIN, YOONGI",
+        place: "Seoul, Korea"
+    },
+    {
+        id: 3,
+        name: "JUNG, HOSEOK",
+        place: "Seoul, Korea"
+    },
+    {
+        id: 3,
+        name: "PARK, JIMIN",
+        place: "Seoul, Korea"
+    },
+    {
+        id: 3,
+        name: "KIM, TAEHYUNG",
+        place: "Seoul, Korea"
+    },
+    {
+        id: 3,
+        name: "JEON, JUNGKOOK",
+        place: "Seoul, Korea"
+    },
+    {
+        id: 3,
+        name: "MAGSINO, LOVELYN",
+        place: "Catigbian, Bohol"
+    },
+    {
+        id: 3,
+        name: "TINAJA, MA. LUISA",
+        place: "San Isidro, Bohol"
+    },
+    {
+        id: 3,
+        name: "VALIENTE, MELROSE",
+        place: "Tip-Tip, Tagbilaran"
+    },
+    {
+        id: 3,
+        name: "COQUILLA, NATHANIEL",
+        place: "Antequera, Bohol"
+    },
+    {
+        id: 3,
+        name: "LAHOY, SHERWIN",
+        place: "Cortes, Bohol"
+    },
+    {
+        id: 3,
+        name: "CASTILLO, HARVIE",
+        place: "Maribijoc, Bohol"
+    },
+    {
+        id: 3,
+        name: "ORING, JEA NORENE",
+        place: "Balilihan, Bohol"
+    },
+    {
+        id: 3,
+        name: "LEE, JI-EUN",
+        place: "Japan, Hanopol"
+    },
+
+]
+
+export default data;
